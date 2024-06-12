9daca25f7d9a6924f0aa075d72e96caf7ece8e92
NivedhaVijayan <30775166+NivedhaVijayan@users.noreply.github.com>
2024-06-12 20:22:27 +0100

    Update TimestampDisplay.js

--- TimestampDisplay.js
+++ TimestampDisplay.js
@@ -4,7 +4,7 @@ import React from 'react';
 const TimestampDisplay = ({ keyphrases, onTimestampClick }) => {
   return (
     <div>
-      <h2>Timestamp Display</h2>
+      <h2>Table of contents- Title with timestamp</h2>
       <ul>
         {keyphrases.map((keyphrase, index) => (
           <li key={index}>
